cbe97e51ff20578120b75a4d2c87c75f877cb7a1
Alex Light <allight@google.com>
2017-10-09 14:40:02 -0700

    Handle default methods without native implementations.
    Starting in Android O some new frameworks methods are implemented as
    default methods. If the jni-bridge doesn't find a native
    implementation it should execute the default implementation, since
    that is expected to be acceptable to the framework in all cases.
    
    Test: build

--- JNIBridge.java
+++ JNIBridge.java
@@ -1,6 +1,8 @@
 package bitter.jnibridge;
 
 import java.lang.reflect.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class JNIBridge
 {
@@ -24,13 +26,63 @@ public class JNIBridge
 
 		public InterfaceProxy(final long ptr) { m_Ptr = ptr; }
 
-		public Object invoke(Object proxy, Method method, Object[] args)
+		private static Object defaultInvoke(Object proxy, Throwable t, Method m, Object[] args) throws Throwable {
+			Class<?> klass = m.getDeclaringClass();
+			if (args == null) {
+				args = new Object[0];
+			}
+			try {
+				Object lookup;
+				Method in_method;
+				Method unreflect_method;
+				Method bind_to_method;
+				Method invoke_with_arguments_method;
+				try {
+					Class<?> lookup_class = Class.forName("java.lang.invoke.MethodHandles$Lookup");
+					Constructor<?> con = lookup_class.getDeclaredConstructor(Class.class, Integer.TYPE);
+					con.setAccessible(true);
+					lookup = con.newInstance(
+							klass, (Integer)lookup_class.getDeclaredField("PRIVATE").get(null));
+					in_method = lookup_class.getDeclaredMethod("in", Class.class);
+					unreflect_method = lookup_class.getDeclaredMethod(
+							"unreflectSpecial", Method.class, Class.class);
+					Class<?> method_handle_class = Class.forName("java.lang.invoke.MethodHandle");
+					bind_to_method = method_handle_class.getDeclaredMethod("bindTo", Object.class);
+					invoke_with_arguments_method =
+							method_handle_class.getDeclaredMethod("invokeWithArguments", List.class);
+				} catch (ReflectiveOperationException r) {
+					// Something went wrong with setup! The expected classes weren't present.
+					t.addSuppressed(r);
+					throw t;
+				}
+				return invoke_with_arguments_method.invoke(
+						bind_to_method.invoke(
+								unreflect_method.invoke(in_method.invoke(lookup, klass), m, klass),
+								proxy),
+						Arrays.asList(args));
+			} catch (InvocationTargetException e) {
+				// Default method was called but it threw an exception!
+				throw e.getCause();
+			}
+		}
+
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
 		{
 			synchronized (m_InvocationLock)
 			{
-				if (m_Ptr == 0)
-					return null;
-				return JNIBridge.invoke(m_Ptr, method.getDeclaringClass(), method, args);
+				try {
+					if (m_Ptr == 0)
+						return null;
+					return JNIBridge.invoke(m_Ptr, method.getDeclaringClass(), method, args);
+				} catch (NoSuchMethodError e) {
+					if ((method.getModifiers() & Modifier.ABSTRACT) == 0) {
+						// We are default. Try to call the default impl.
+						return defaultInvoke(proxy, e, method, args);
+					} else {
+						// Doesn't seem to be a default method!
+						throw e;
+					}
+				}
 			}
 		}
 
